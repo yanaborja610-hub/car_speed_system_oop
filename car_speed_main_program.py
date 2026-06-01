e9d4ecba0e1ee1bbdd3062fa9d866da4ca958437
yanab <yanaborja610@gmail.com>
2026-06-01 18:57:46 +0800

    Added the display for the acceleration speed

--- car_speed_main_program.py
+++ car_speed_main_program.py
@@ -6,3 +6,12 @@ make = int(input("Car brand:"))
 my_car = Car(year, make)
 
 my_car.display_header()
+
+print("\nAcceleration Phase")
+print("-" * 50)
+
+for lap in range(1,6):
+    my_car.accelerate()
+    print(f"Lap {lap}: Speed = {my_car.get_speed()} mph")
+
+print("\nMax speed reached!")

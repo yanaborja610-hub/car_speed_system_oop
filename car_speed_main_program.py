64da4f61e8338ad55c1c52d05d81eca25aea664b
yanab <yanaborja610@gmail.com>
2026-06-01 19:00:57 +0800

    Added the display for the braking phase

--- car_speed_main_program.py
+++ car_speed_main_program.py
@@ -7,6 +7,7 @@ my_car = Car(year, make)
 
 my_car.display_header()
 
+# Acceleration
 print("\nAcceleration Phase")
 print("-" * 50)
 
@@ -15,3 +16,14 @@ for lap in range(1,6):
     print(f"Lap {lap}: Speed = {my_car.get_speed()} mph")
 
 print("\nMax speed reached!")
+
+# Brake
+print("\nBraking Phase")
+print("-" * 50)
+
+for brake in range(1,6):
+    my_car.brake()
+    print(f"Brake {brake}: Speed = {my_car.get_speed()} mph")
+
+print("\nCar stopped.")
+print("=" * 50)
